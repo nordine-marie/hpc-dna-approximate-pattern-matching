d1b18aaf1a20258b93fd2fd3eca9bfd12e3110e3
Nordine MARIE <nordine.marie@telecom-sudparis.eu>
2021-11-05 20:11:44 +0100

    fix script

--- src/apm_mpi/test.sh
+++ src/apm_mpi/test.sh
@@ -1,7 +1,7 @@
 make clean
 make
 echo "___________________MPI_APM_Below__________________"
-mpirun -np 2 apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGG
+mpirun -np 2 ./apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGGAAAAAAAAAA
 echo "___________________Original_APM_Below______________________"
-../apm_original/apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGG
+../apm_original/apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGGAAAAAAAAAA
 
