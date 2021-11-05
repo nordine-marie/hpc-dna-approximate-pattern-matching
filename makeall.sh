5e4b228625e86d4029e4f4ab229514b20d18baa4
Nordine MARIE <nordine.marie@telecom-sudparis.eu>
2021-11-05 22:50:24 +0100

    fix script

--- makeall.sh
+++ makeall.sh
@@ -9,28 +9,28 @@ cd ../
 
 #APM OpenMP
 echo "making the OpenMP apm ==============="
-cd apm/apm_omp
+cd apm_omp
 make clean
 make
 cd ../
 
 #APM MPI
 echo "making the MPI apm ==============="
-cd apm/apm_mpi
+cd apm_mpi
 make clean
 make
 cd ../
 
 #APM OpenMP + MPI
 echo "making the original OpenMP + MPI apm ==============="
-cd apm/apm_omp_mpi
+cd apm_omp_mpi
 make clean
 make
 cd ../
 
 #APM Cuda
 echo "making the original CUDA apm ==============="
-cd apm/apm_cuda/src
+cd apm_cuda/src
 make clean
 make
 cd ../../
