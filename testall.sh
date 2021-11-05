decfa6856666fe0528e72224807ac1ab0aa0b073
Nordine MARIE <nordine.marie@telecom-sudparis.eu>
2021-11-05 23:20:11 +0100

    fix test script

--- testall.sh
+++ testall.sh
@@ -2,21 +2,21 @@
 
 #APM original
 echo "original apm ==============="
-./apm/apm_original/apm 5 ../../dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGGAAAAAAAAAA
+./apm/apm_original/apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGGAAAAAAAAAA
 
 #APM OpenMP
 echo "OpenMP apm ==============="
-./apm/apm_omp/apm 5 ../../dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGGAAAAAAAAAA
+./apm/apm_omp/apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGGAAAAAAAAAA
 
 #APM MPI
 echo "MPI apm ==============="
-mpirun -np 3 ./apm/apm_mpi/apm 5 ../../dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGGAAAAAAAAAA
+mpirun -np 3 ./apm/apm_mpi/apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGGAAAAAAAAAA
 
 #APM OpenMP + MPI
 echo "OpenMP + MPI apm ==============="
-mpirun -np 3 ./apm/apm_omp_mpi/apm 5 ../../dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGGAAAAAAAAAA
+mpirun -np 3 ./apm/apm_omp_mpi/apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGGAAAAAAAAAA
 
 #APM Cuda
 echo "CUDA apm ==============="
-./apm/apm_omp/src/apm 5 ../../../dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGGAAAAAAAAAA
+./apm/apm_cuda/src/apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGGAAAAAAAAAA
 
