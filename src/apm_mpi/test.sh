95824bbf2237e78d0b5edc0ae4f3809aca73850b
Nordine MARIE <nordine.marie@telecom-sudparis.eu>
2021-11-05 17:56:35 +0100

    quick edit

--- src/apm_mpi/test.sh
+++ src/apm_mpi/test.sh
@@ -1,7 +1,7 @@
 make clean
 make
-echo "MPI APM"
-mpirun -np 2 apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTT
+echo "___________________MPI_APM_Below__________________"
+mpirun -np 2 apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGG
 echo "___________________Original_APM_Below______________________"
-../apm_original/apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTT
+../apm_original/apm 5 ./dna/chr1_KI270763v1_alt.fa AGTTTTTTTTTTTTTTTTTTGG
 
