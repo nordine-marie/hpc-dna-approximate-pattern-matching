452ae2fea37ed915031c5a747febc63f4e2e0eb4
Nordine MARIE <nordine.marie@telecom-sudparis.eu>
2021-11-08 07:57:00 +0100

    cuda test

---
+++ apm/apm_cuda/src/test_cuda.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+distance=$1
+dna=$2
+char="A"
+pattern=""
+for i in {1..1000}; do 
+    pattern="$pattern$char"
+    ../apm_original/apm $distance $dna $pattern | grep "APM done in" | awk '{ printf $4 }' >> out_cuda
+    printf ";" >> out_cuda
+    ./apm $distance $dna $pattern | grep "APM done in" | awk '{ printf $4 }' >> out_cuda
+    printf "\n" >> out_cuda
+done
